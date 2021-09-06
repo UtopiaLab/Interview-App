417046a8f9379397dec7d5c59f077ec6c7b1c569
UtopiaLab <89520@fhss.sjp.ac.lk>
2021-09-06 11:52:44 +0530

    index page creation

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php
+<html lang="en">
+
+<head>
+    <title>Interview App</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+</html>
+<body>
+    <h1>Welcome</h1>
+    <p>Please <a href="login.php">Login</a> or <a href="signup.php">Signup</a> to continue!</p>
+</body>
