ce35fac763ecb477697c299f467f8c43c7525359
UtopiaLab <89520@fhss.sjp.ac.lk>
2021-09-07 14:05:56 +0530

    login functionality

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <html lang="en">
 
 <head>
@@ -7,6 +11,18 @@
 
 <body>
     <h1>Welcome</h1>
+
+<?php
+    if (isset($_SESSION["logoutSuccess"])) {
+        echo "<div>" . $_SESSION["logoutSuccess"] . "</div>";
+        session_destroy();
+    }
+    if (isset($_SESSION["noLogin"])) {
+        echo "<div>" . $_SESSION["noLogin"] . "</div>";
+        session_destroy();
+    }
+?>
+
     <p>Please <a href="login.php">Login</a> or <a href="signup.php">Signup</a> to continue!</p>
 </body>
 
